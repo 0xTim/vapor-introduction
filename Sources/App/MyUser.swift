9e12b93e07dcbeae44b64e6e4ce2f8660c38b7a9
Tim <0xtimc@gmail.com>
2017-03-06 13:01:49 +0000

    Add example of authentication

--- Sources/App/MyUser.swift
+++ Sources/App/MyUser.swift
@@ -1,36 +1,98 @@
 import Fluent
 import Vapor
+import Auth
+import BCrypt
 
 class MyUser: Model {
     var id: Node?
     var exists: Bool = false
     
     var name: String
+    var password: String
     
-    init(name: String) {
+    init(name: String, password: String) {
         self.name = name
+        self.password = password
     }
     
     required init(node: Node, in context: Context) throws {
         id = try node.extract("id")
         name = try node.extract("name")
+        password = try node.extract("password")
     }
     
     func makeNode(context: Context) throws -> Node {
-        return try Node(node: [
+        var node = try Node(node: [
             "id": id,
             "name": name
             ])
+        
+        switch context {
+        case is DatabaseContext:
+            node["password"] = password.makeNode()
+        default:
+            break
+        }
+    
+        return node
     }
     
     static func prepare(_ database: Database) throws {
         try database.create(self.entity) { users in
             users.id()
             users.string("name")
+            users.string("password")
         }
     }
     
     static func revert(_ database: Database) throws {
         try database.delete(self.entity)
+    }
+}
+
+extension MyUser: Auth.User {
+    convenience init(credentials: NamePassword) throws {
+        try self.init(name: credentials.name, password: BCrypt.digest(password: credentials.password))
+    }
+    
+    static func register(credentials: Credentials) throws -> Auth.User {
+        guard let namePassword = credentials as? NamePassword else {
+            throw Abort.custom(status: .forbidden, message: "Unsupported credentials type \(type(of: credentials))")
+        }
+        
+        let user = try MyUser(credentials: namePassword)
+        return user
+    }
+    
+    static func authenticate(credentials: Credentials) throws -> Auth.User {
+        switch credentials {
+        case let namePassword as NamePassword:
+            guard let user = try MyUser.query().filter("name", namePassword.name).first() else {
+                throw Abort.custom(status: .networkAuthenticationRequired, message: "Invalid username or password")
+            }
+            if try BCrypt.verify(password: namePassword.password, matchesHash: user.password) {
+                return user
+            }
+            else {
+                throw Abort.custom(status: .networkAuthenticationRequired, message: "Invalid username or password")
+            }
+        case let id as Identifier:
+            guard let user = try MyUser.find(id.id) else {
+                throw Abort.custom(status: .forbidden, message: "Invalid user identifier")
+            }
+            return user
+        default:
+            throw Abort.custom(status: .forbidden, message: "Unsupported credentials type \(type(of: credentials))")
+        }
+    }
+}
+
+struct NamePassword: Credentials {
+    let name: String
+    let password: String
+    
+    public init(name: String, password: String) {
+        self.name = name
+        self.password = password
     }
 }
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,11 +1,16 @@
 import Vapor
 import VaporMemory
+import HTTP
+import Auth
 
 let drop = Droplet()
 
 try drop.addProvider(VaporMemory.Provider.self)
 
 drop.preparations.append(MyUser.self)
+
+let authMiddleware = AuthMiddleware(user: MyUser.self)
+drop.middleware.append(authMiddleware)
 
 drop.get { req in
     return "Hello World!"
@@ -28,11 +33,13 @@ drop.post("hi") { request in
 }
 
 drop.post("users") { request in
-    guard let name = request.data["name"]?.string else {
+    guard let name = request.data["name"]?.string, let password = request.data["password"]?.string else {
         throw Abort.badRequest
     }
     
-    var newUser = MyUser(name: name)
+    
+    let credentials = NamePassword(name: name, password: password)
+    var newUser = try MyUser(credentials: credentials)
     try newUser.save()
     
     return try newUser.makeJSON()
@@ -40,6 +47,42 @@ drop.post("users") { request in
 
 drop.get("users") { request in
     return try MyUser.all().makeJSON()
+}
+
+drop.get("login") { request in
+    return try drop.view.make("login")
+}
+
+drop.post("login") { request in
+    guard let name = request.data["name"]?.string, let password = request.data["password"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    let credentials = NamePassword(name: name, password: password)
+    
+    do {
+        try request.auth.login(credentials)
+        
+        guard let _ = try request.auth.user() as? MyUser else {
+            request.storage.removeValue(forKey: "remember_me")
+            throw Abort.badRequest
+        }
+        return Response(redirect: "/protected")
+    }
+    catch {
+        print(error)
+        return try drop.view.make("login")
+    }
+}
+
+let protected = Auth.LoginRedirectMiddleware(loginRoute: "login")
+
+drop.grouped(protected).get("protected") { request in
+    guard let user = try request.auth.user() as? MyUser else {
+        throw Abort.custom(status: .forbidden, message: "You must be logged in")
+    }
+    
+    return "Hello \(user.name)"
 }
 
 drop.run()
